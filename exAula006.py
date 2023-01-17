f2aa6f108065197d29af363d9e26a0d3226419cd
atnzpe <gleysonasilva@gmail.com>
2023-01-17 19:04:56 -0300

    Cria o Desafio 003

---
+++ exAula006.py
@@ -0,0 +1,7 @@
+# Digite dois numero e apresente a soma deles
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+
+print(f'A soma de {n1}, mais {n2} é igual a {s}!')
