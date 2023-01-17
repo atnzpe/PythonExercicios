e50df9bba8e9b89f5f6bba876eaf3c08058b3f38
atnzpe <gleysonasilva@gmail.com>
2023-01-17 17:41:52 -0300

    Cria o Exercicio002

---
+++ ex002.py
@@ -0,0 +1,8 @@
+# Desafio 002
+# Faça um programa que leia o nome da pessoa, e retorne uma mensagem de boas vindas!
+
+nome = input('Qual o seu nome? ')
+
+msg = f'Seja bem vindo, {nome}!'
+
+print(msg)
